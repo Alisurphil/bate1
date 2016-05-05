4dec39eafe9bd0b38ada4e1222ccf94519ca0f32
谢雪彬 <q131968850@qq.com>
2016-05-05 09:21:12 +0800

    环信

--- beta1/Pods/Target Support Files/Pods/Pods-resources.sh
+++ beta1/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -58,9 +58,11 @@ install_resource()
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "EaseMobSDKFull/EaseMobSDKFull/resources/EaseMob.bundle"
   install_resource "TAOverlay/TAOverlay/TAOverlay.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "EaseMobSDKFull/EaseMobSDKFull/resources/EaseMob.bundle"
   install_resource "TAOverlay/TAOverlay/TAOverlay.bundle"
 fi
 
